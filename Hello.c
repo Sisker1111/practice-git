acb4eafae5b63edda90d736466a658cdf1717254
harry1120 <harry8fan@gmail.com>
2020-09-10 17:56:18 +0800

    Init Hello.c

--- Hello.c
+++ Hello.c
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("4444new hello github\n");
+	printf("555new hello github\n");
 	return 0;
 }
